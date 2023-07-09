325e305dd001a9a0706b2012bf69537c8f974f96
Babuuum <85997371+Babuuum@users.noreply.github.com>
2023-07-09 11:43:59 +0300

    release

--- evakyator_v2/js/main.js
+++ evakyator_v2/js/main.js
@@ -24,6 +24,15 @@ $(document).ready(function() {
  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
  }).addTo(map);
 
+ var geocoderOptions = {
+   geocoderOptions: {
+     nominatim: {
+       params: {
+         city: 'Санкт-Петербург' 
+       }
+     }
+   }
+ };
 
  var control = L.Routing.control({
  position: 'topleft',
@@ -31,7 +40,7 @@ $(document).ready(function() {
    alternatives: true
  }),
  routeWhileDragging: true,
- geocoder: L.Control.Geocoder.nominatim()
+ geocoder: L.Control.Geocoder.nominatim(geocoderOptions)
  }).addTo(map);
 
  var center1 = L.latLng(59.94, 30.25);
@@ -46,30 +55,31 @@ $(document).ready(function() {
  var isInArea2;
 
  var distance = document.getElementById("distance");
- let path;
+ let epath;
  control.on('routesfound', function(e) {
  var route = e.routes[0];
- path = (route.summary.totalDistance/1000).toFixed(2)
- console.log(path)
+ epath = (route.summary.totalDistance/1000).toFixed(2)
+ console.log(epath)
  distance.innerText = "Расстояние: " + (route.summary.totalDistance/1000).toFixed(2) + " км";
  isInArea1 = area1.getBounds().contains(route.coordinates[0]) && area1.getBounds().contains(route.coordinates[route.coordinates.length - 1]);
  isInArea2 = area2.getBounds().contains(route.coordinates[0]) && area2.getBounds().contains(route.coordinates[route.coordinates.length - 1]);
  console.log( isInArea1, isInArea2)
  });
-console.log(path)
+
 document.getElementById("calculate-btn").addEventListener("click", function calculateCost()
     {
        var cost = document.getElementById("order-details")
-       var blockedWheels;
-       let rpath = path
+       let blockedWheels;
+       let rpath = epath
        var carType = document.getElementById("car-type").value;
-          if (carType === "sheet") { rpath = path * 0.9};
-          if (carType === "medium") { rpath = path * 1.2};
-          if (carType === "moto") { rpath = path * 0.8};
-          if (carType === "big") { rpath = path * 1.5};
-          if (carType === "Lbig") { rpath = path * 2};
-          if (carType === "XLbig") { rpath = path * 3};              
-       var blocked = document.getElementById("car-type").value;
+          if (carType === "sheet") { rpath = epath * 0.9};
+          if (carType === "medium") { rpath = epath * 1.2};
+          if (carType === "moto") { rpath = epath * 0.8};
+          if (carType === "big") { rpath = epath * 1.5};
+          if (carType === "Lbig") { rpath = epath * 2};
+          if (carType === "XLbig") { rpath = epath * 3};              
+       var blocked = document.getElementById("blocked").value;
+         console.log(blocked, carType)
          if (blocked === "zero") {blockedWheels = 0};
          if (blocked === "one") {blockedWheels = 500};
          if (blocked === "two") {blockedWheels = 1000};
@@ -78,6 +88,7 @@ document.getElementById("calculate-btn").addEventListener("click", function calc
        var ditch = document.getElementById("ditch").checked ? 750 : 0;
        var night = document.getElementById("night").checked ? 0.8 : 1;
        var loading = isInArea2 == true ? 3500 : 2500;
+       console.log(epath, rpath, night, ditch, blockedWheels, blocked, loading)
        if (isInArea1 == true) {
          if (document.getElementById("payment-method").value === "hourly"){
             cost.innerText = "Цена за час: " + (2500) + ", погрузка будет стоить: " + (loading + ditch + blockedWheels)
@@ -88,21 +99,3 @@ document.getElementById("calculate-btn").addEventListener("click", function calc
       }
     }
 );
- // ograni4it' koli4estvo to4ek na karte
- //vnedrit' otpravky emailov i ograni4it' do 2 v 4as s odnogo ip
- //vi3vat evik
-// gl9nyt' hostingi i domeni s podderjkoi rassilok php i td
-
-//napisat' 4to 9 sdelal plus minys i 4to ispol'3oval videlit' potnie dl9 seb9 momenti
-// vnesti po4ty
-//so3dat' server osrm ili ispol'3ovat' beslatnii server s bol'shim koli4estvom 3aprosov
-//conf file
-
-//osnovana9 problema bila v tom 4to nepravel'no podklu4enni biblioetki
-//domen rm-evakyator.ru
-
-// !!!!!perepisat' ra3vivai svoe ymenie stryktyrirovat'
-// !!!!perepisat': ra3nie isto4niki infi i kyrsi daje ludi naceleni na ra3nie celi naprimer, podgotovka k praktike ili je obshee ra3vitie i td
-// pon9t' na 4em ly4she sosredoto4its9 pri i3y4enii python
-//formirovat' dover9nnie resyrsi i y4it' dokymentaciu
-// dokymentiryi to 4to u3al, vipisivai vsu vajnyu hyinu i3 proekta
